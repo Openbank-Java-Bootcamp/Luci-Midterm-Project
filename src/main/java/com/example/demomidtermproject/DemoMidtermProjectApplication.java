1b3b1a118f0695499fb394d0d6d088e4817f70d9
Lucia Sanchez <luci.sanchez.ortiz@gmail.com>
2022-05-16 09:21:08 +0200

    Init

---
+++ src/main/java/com/example/demomidtermproject/DemoMidtermProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demomidtermproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMidtermProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMidtermProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demomidtermproject/DemoMidtermProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demomidtermproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMidtermProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
